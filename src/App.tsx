e6a151f428d9a2ab6db8a9ad1bf30b43a98068e1
Romulo Tone <romulo.tone@cevt.se>
2023-03-08 20:14:45 +0100

    fix redux browser tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./store/store";
 import Scene from './components/Scene';
 
 function App() {
   return (
-    <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <div style={{ width: "100%", height: "100%" }}>
-        <Scene />
-      </div>
-      {/* </PersistGate> */}
-    </Provider>
+    <Suspense fallback={<div>Loading... </div>}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <div style={{ width: "100%", height: "100%" }}>
+            <Scene />
+          </div>
+        </PersistGate>
+      </Provider>
+    </Suspense>
+
 
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, Store, compose, combineReducers } from "redux";
 // import thunk from "redux-thunk";
 import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
+import storage from "redux-persist/lib/storage";
 import { IBaseReducer } from "./base-types";
 import { ISquaretModel } from "../models/square-model";
 import cellReducer from "./cell/cell-reducer"
@@ -10,24 +10,22 @@ export interface ApplicationState {
     cellReducer: IBaseReducer<ISquaretModel>
 }
 
-// const persistConfig = {
-//     key: "root",
-//     storage: storage,
-//     whitelist: ["cellReducer"],
-// };
+const persistConfig = {
+    key: "root",
+    storage: storage,
+    whitelist: ["cellReducer"],
+};
 
 const composeEnhancers =
     (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// const presistedReducer = persistReducer(persistConfig, combineReducers({
-//     cellReducer
-// }));
+const presistedReducer = persistReducer(persistConfig, combineReducers({
+    cellReducer
+}));
 
 const store: Store<ApplicationState> = createStore(
-    combineReducers({
-        cellReducer
-    }),
-    // composeEnhancers(applyMiddleware(thunk))
+    presistedReducer,
+    composeEnhancers()
 );
 
 const persistor = persistStore(store);
